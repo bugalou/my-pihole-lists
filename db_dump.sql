8354efc9b6ccfdfbec06dce2628b678aaa75bc23
bugalou <hgbugalou@gmail.com>
2024-05-27 03:00:02 +0000

    Updated May 27, 2024  3:00AM

--- db_dump.sql
+++ db_dump.sql
@@ -75,7 +75,7 @@ CREATE TABLE adlist
 	invalid_domains INTEGER NOT NULL DEFAULT 0,
 	status INTEGER NOT NULL DEFAULT 0
 );
-INSERT INTO adlist VALUES(1,'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',1,1641325623,1641325623,'Migrated from /etc/pihole/adlists.list',1716666866,138003,1,1);
+INSERT INTO adlist VALUES(1,'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',1,1641325623,1641325623,'Migrated from /etc/pihole/adlists.list',1716698282,138003,1,2);
 CREATE TABLE domainlist_by_group
 (
 	domainlist_id INTEGER NOT NULL REFERENCES domainlist (id),
